3df5dbb4518a4cb9a2fa9396cdee83b285f74a30
surendra-ayasya <surendra.rathore@ayasya.com>
2025-05-20 15:25:36 +0530

    Update app.py
    add the port number

--- app.py
+++ app.py
@@ -152,4 +152,4 @@ def serve_image(filename):
         return jsonify({"error": "Image not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000)
