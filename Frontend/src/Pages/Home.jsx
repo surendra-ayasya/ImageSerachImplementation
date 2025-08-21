325479f5ab0c2724d1b50f4b8498663d3cd7ace0
KushagraGoel10 <goelkushagra2000@gmail.com>
2025-08-21 10:24:47 +0530

    latest changes

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,14 +1,8 @@
-import Footer from "../Components/Footer/Footer";
-import Header from "../Components/Header/Header";
-import ImageUpload from "../Components/ImageSearch/ImageUpload";
-import SearchInput from "../Components/ImageSearch/SearchInput";
 import TileUploader from "../Components/ImageSearch/UploadTiles";
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-gray-50 px-4 py-10">
-
-      
       <div className="mt-16">
         <TileUploader />
       </div>
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -14,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ImageProvider>
       <KajariaSearch />
+      
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/results" element={<Results />} />
