44388b2665c5cd5b7cd811432363fc7193daf5e0
surendra-ayasya <surendra.rathore@ayasya.com>
2025-09-05 16:33:12 +0530

    S3 Bucket Changes
    Changes done

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 import torch
 # AWS configuration (read from environment, not hardcoded!)
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID","AKIA5G2VGIXXKHHEUDD3")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY","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")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID","")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY","")
 AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION", "ap-south-1")
 AWS_BUCKET = os.getenv("AWS_BUCKET", "kclwebsitebucket")
 # S3 Base URL
